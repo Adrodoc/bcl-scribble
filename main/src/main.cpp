cd8b9f9e54019271cd0a6f4fe5a1c9265779b784
Adrodoc <adrodoc55@googlemail.com>
2021-03-05 17:31:13 +0100

    Fix calculation for node_id

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -5,12 +5,14 @@
 int main(int argc, char *argv[])
 {
   BCL::init();
+  int rank = BCL::rank();
+  int nprocs = BCL::nprocs();
 
   MPI_Comm local_comm; // Communicator for inside local node
   MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &local_comm);
   int local_rank; // Rank inside local node
   MPI_Comm_rank(local_comm, &local_rank);
-  int node_id = local_rank; // The unique ID of the current node
+  int node_id = rank; // The unique ID of the current node
   MPI_Bcast(&node_id, 1, MPI_INT, 0, local_comm);
   MPI_Comm_free(&local_comm);
   char processor_name_array[MPI_MAX_PROCESSOR_NAME];
@@ -22,8 +24,6 @@ int main(int argc, char *argv[])
   McsLock lock;
 
   lock.acquire();
-  int rank = BCL::rank();
-  int nprocs = BCL::nprocs();
   log() << "lock acquired" << std::endl;
   // sleep(1);
   log() << "releasing lock" << std::endl;
