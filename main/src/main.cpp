f9abff90787986f9d3c33a677405d39bb2572afb
Adrodoc <adrodoc55@googlemail.com>
2021-03-05 17:19:23 +0100

    Calculate ID for node

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -6,11 +6,14 @@ int main(int argc, char *argv[])
 {
   BCL::init();
 
-  MPI_Comm local_comm;
+  MPI_Comm local_comm; // Communicator for inside local node
   MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &local_comm);
-  int local_rank;
+  int local_rank; // Rank inside local node
   MPI_Comm_rank(local_comm, &local_rank);
-  log() << "local_rank=" << local_rank << std::endl;
+  int node_id = local_rank; // The unique ID of the current node
+  MPI_Bcast(&node_id, 1, MPI_INT, 0, local_comm);
+  MPI_Comm_free(&local_comm);
+  log() << "local_rank=" << local_rank << "node_id=" << node_id << std::endl;
 
   McsLock lock;
 
