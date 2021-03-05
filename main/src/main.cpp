07e7223237280d0030027564a5aae38c6fad30e6
Adrodoc <adrodoc55@googlemail.com>
2021-03-05 17:00:40 +0100

    Print local rank

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -6,6 +6,12 @@ int main(int argc, char *argv[])
 {
   BCL::init();
 
+  MPI_Comm local_comm;
+  MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &local_comm);
+  int local_rank;
+  MPI_Comm_rank(local_comm, &local_rank);
+  log() << "local_rank=" << local_rank << std::endl;
+
   McsLock lock;
 
   lock.acquire();
